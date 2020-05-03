511c16f7f4c0c0d2b07aa5f3b0d0b98316fd4666
akiel <diazorozcoj@gmail.com>
2020-05-04 00:28:46 +0200

    Add basic authentication to the service

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
+import logging
 import os
 
 import docker
 from aiohttp import web
+from aiohttp_basicauth import BasicAuthMiddleware
 
 DOCKER_IMAGE = os.getenv('DOCKER_IMAGE', 'guamulo/openvpn')
 OVPN_DATA = os.getenv('OVPN_DATA', f'{os.getcwd()}/ovpn-data')
+SERVICE_USER = os.getenv('SERVICE_USER')
+SERVICE_PASSWORD = os.getenv('SERVICE_PASSWORD')
 
 
 async def handle_create(request):
@@ -36,9 +40,14 @@ async def handle_healthcheck(request):
 
 
 if __name__ == '__main__':
-    app = web.Application()
+    if SERVICE_USER and SERVICE_PASSWORD:
+        auth = BasicAuthMiddleware(username=SERVICE_USER, password=SERVICE_PASSWORD)
+        app = web.Application(middlewares=[auth])
+    else:
+        logging.warning('service is running without authentication')
+        app = web.Application()
+
     app.add_routes([web.post('/create', handle_create)])
     app.add_routes([web.post('/revoke', handle_revoke)])
     app.add_routes([web.get('/healthcheck', handle_healthcheck)])
-
     web.run_app(app, host='0.0.0.0', port=5000)
