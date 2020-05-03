ae09c634e5cd2504ddb7a081da7b114767301f52
akiel <diazorozcoj@gmail.com>
2020-05-04 00:38:39 +0200

    Add method to get the config for a single user

--- server.py
+++ server.py
@@ -26,13 +26,26 @@ async def handle_create(request):
 
 
 async def handle_revoke(request):
-    post = await request.json()
-    user = post.get('user')
-    client = docker.from_env()
-    client.containers.run(image=DOCKER_IMAGE, command=f"bash -c \"echo 'yes' | ovpn_revokeclient {user}\"",
-                          remove=True,
-                          volumes={OVPN_DATA: {'bind': '/etc/openvpn', 'mode': 'rw'}})
+    user = request.match_info.get('user', "")
+    if user != "":
+        client = docker.from_env()
+        client.containers.run(image=DOCKER_IMAGE, command=f"bash -c \"echo 'yes' | ovpn_revokeclient {user}\"",
+                              remove=True,
+                              volumes={OVPN_DATA: {'bind': '/etc/openvpn', 'mode': 'rw'}})
+    else:
+        return web.Response(status=404)
     return web.Response(text='ok')
+
+
+async def handle_get(request):
+    user = request.match_info.get('user', "")
+    client = docker.from_env()
+    output = client.containers.run(image=DOCKER_IMAGE, command=f"ovpn_getclient {user}",
+                                   remove=True,
+                                   detach=False,
+                                   volumes={OVPN_DATA: {'bind': '/etc/openvpn', 'mode': 'rw'}},
+                                   stdout=True)
+    return web.Response(text=output.decode('ascii'))
 
 
 async def handle_healthcheck(request):
@@ -47,7 +60,8 @@ if __name__ == '__main__':
         logging.warning('service is running without authentication')
         app = web.Application()
 
-    app.add_routes([web.post('/create', handle_create)])
-    app.add_routes([web.post('/revoke', handle_revoke)])
+    app.add_routes([web.post('/', handle_create)])
+    app.add_routes([web.delete('/{user}', handle_revoke)])
+    app.add_routes([web.get('/{user}', handle_get)])
     app.add_routes([web.get('/healthcheck', handle_healthcheck)])
     web.run_app(app, host='0.0.0.0', port=5000)
